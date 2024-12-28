8f5f09e96ceb9fd80eded29275fcfc4d00195d8e
rajeev-git-space <rajeevguptapro7@gmail.com>
2024-12-28 16:44:08 +0530

    removed backend folder & made changes in yaml to handle CICD for serverless changes only

--- backend/src/utils/response.js
+++
@@ -1,2 +0,0 @@
-exports.successResponse = (res, data, status = 200) => res.status(status).json({ success: true, data });
-exports.errorResponse = (res, message, status = 500) => res.status(status).json({ success: false, error: message });
