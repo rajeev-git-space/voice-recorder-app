899d0f6deb80a3641a662dfc4cbb8383d67b10ab
rajeev-git-space <rajeevguptapro7@gmail.com>
2024-12-28 16:22:13 +0530

    inclusion of recording_name in listing

--- backendServerless/helpers/retrieveAudio.mjs
+++ backendServerless/helpers/retrieveAudio.mjs
@@ -7,12 +7,12 @@ export const retrieveAudio = async () => {
     try {
         const bucketName = 'ec-voice-recorder-app';
         const response = await s3.send(new ListObjectsV2Command({ Bucket: bucketName, Prefix: 'merged-audio/' }));
-        console.log('Response:', response);
+
         if (!response.Contents?.length) {
             throw new Error("No Audio Saved!");
         }
         const files = response.Contents.map((item) => ({
-            key: item.Key,
+            recording_name: item.Key.split('/').pop().split('.').shift(),
             url: `https://${bucketName}.s3.amazonaws.com/${item.Key}`,
             lastModified: item.LastModified,
             size: item.Size,
