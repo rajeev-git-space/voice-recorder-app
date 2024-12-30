41591fc292458b516c9d7649361e1b0feadd1969
rajeev-git-space <rajeevguptapro7@gmail.com>
2024-12-30 14:27:52 +0530

    removed non-relevant code from backend

--- frontend/src/utils/validationUtils.js
+++ frontend/src/utils/validationUtils.js
@@ -1,4 +1,3 @@
-// src/utils/validationUtils.js
 export const validateChunkDuration = (duration) => {
   if (isNaN(duration) || duration < 1 || duration > 40) {
     throw new Error('Chunk duration must be between 1 and 40 seconds.');
